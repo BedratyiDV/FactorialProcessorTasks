548ce799956e0f27b2e3c81257747d5804161b6c
BedratyiDV <bedrik2023@gmail.com>
2023-11-17 12:29:00 +0100

    The First Commit
    17/11/2023

---
+++ FactorialProcessorTasks/FactorialProcessorTasks.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialProcessorTasks
+{
+    public class FactorialProcessorTasks
+    {
+        private Stopwatch _stopwatch;
+        private int _maxVal;
+        private int _passedNum;
+        private object _lock = new();
+
+        public FactorialProcessorTasks()
+        {
+            _stopwatch = new();
+        }
+        
+        public void MathFactorial(int n, bool isParallel, CancellationToken token) 
+        {
+          _maxVal = n;
+            if (isParallel)
+            {
+                StartFactorialParallel(n, token);
+            }
+            else 
+            { 
+                StartFactorialSequential(n); 
+            }
+
+            void StartFactorialSequential(int param)
+            { 
+                _stopwatch.Start();
+                for (int i = 0; i < param; i++) 
+                
+                {
+                    PrintFactorial(i);
+                }
+            }
+
+            void StartFactorialParallel(int param, CancellationToken token) 
+            
+            {
+                _stopwatch.Start();
+                for (int i = 0; i < param; i++)
+
+                {
+                    int n = i;
+                    PrintFactorial(i);
+                    new Task(PrintFactorial, n, token).Start();
+                }
+            }
+             void PrintFactorial(object? n)
+            
+            {
+                int param = (int)n;
+                var result = Factorial(param);
+                Console.WriteLine($"Fact({param} is {result}\t\t");
+                lock ( _lock ) 
+                
+                {
+                    if (++_passedNum == _maxVal)
+                    {
+                        _stopwatch.Stop();
+                        Console.WriteLine($"ms:\t{_stopwatch.ElapsedMilliseconds}");
+                        Console.WriteLine($"ticks:\t{_stopwatch.ElapsedTicks}\n\n");
+                    }
+                }
+            }
+             static int Factorial(int n)
+            
+            {
+                Thread.Sleep( 1000 );
+                if(n==0 || n==1) return 1;
+                return n*Factorial(n-1);
+            }
+
+        }
+    }
+}
---
+++ FactorialProcessorTasks/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace FactorialProcessorTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();    
+            var token = cancellationTokenSource.Token;
+
+            FactorialProcessorTasks factorialProcessorTasks1 = new FactorialProcessorTasks();
+            factorialProcessorTasks1.MathFactorial(15, true, token);
+            Thread.Sleep(2000);
+
+            while (true) 
+            
+            {
+                var key = Console.ReadLine();
+                if (key == "q")
+                
+                {
+                    cancellationTokenSource.Cancel();
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
